ea4d8cbb5a24088c69f4907eec8ff606796e2ddf
luxiaoxun <luxiaoxun001@qq.com>
2020-07-17 09:57:40 +0800

    Update log aspect

--- eagle-api/src/main/java/com/alarm/eagle/api/config/LogAspect.java
+++ eagle-api/src/main/java/com/alarm/eagle/api/config/LogAspect.java
@@ -14,34 +14,41 @@ import org.springframework.context.annotation.Configuration;
 /**
  * Created by luxiaoxun on 18/1/3.
  */
-//@Aspect
-//@Configuration
+@Aspect
+@Configuration
 public class LogAspect {
     private static final Logger logger = LoggerFactory.getLogger(LogAspect.class);
 
     @Pointcut("@annotation(org.springframework.web.bind.annotation.PostMapping)")
-    public void postMappingPointCut() {}
+    public void postMappingPointCut() {
+    }
 
     @Pointcut("@annotation(org.springframework.web.bind.annotation.GetMapping)")
-    public void getMappingPointCut() {}
+    public void getMappingPointCut() {
+    }
 
     @Pointcut("@annotation(org.springframework.web.bind.annotation.RequestMapping)")
-    public void requestMappingPointCut() {}
+    public void requestMappingPointCut() {
+    }
 
     @Around("postMappingPointCut() || getMappingPointCut() || requestMappingPointCut()")
     public Object logControllerInAndOut(ProceedingJoinPoint joinPoint) throws Throwable {
         Object result;
         long start = System.currentTimeMillis();
         Signature signature = joinPoint.getSignature();
-        String param = JsonUtil.encode(joinPoint.getArgs());
-        try{
+        String param = "";
+        //Can not serialize BasicErrorController with json format
+        if (!signature.getName().equals("error")) {
+            param = JsonUtil.encode(joinPoint.getArgs());
+        }
+        try {
             result = joinPoint.proceed();
             long end = System.currentTimeMillis();
-            logger.info("method={}||execute_time={}||param={}||result={}", signature.toString(), end-start, param, JsonUtil.encode(result));
-        }catch (Exception e) {
+            logger.info("method={}||execute_time={}||param={}||result={}", signature.toString(), end - start, param, JsonUtil.encode(result));
+        } catch (Exception e) {
             long end = System.currentTimeMillis();
             result = ResponseUtil.fail();
-            logger.error("method={}||execute_time={}||param={}||error=", signature.toString(), end-start, param, e);
+            logger.error("method={}||execute_time={}||param={}||error=", signature.toString(), end - start, param, e);
         }
         return result;
     }
@@ -52,13 +59,13 @@ public class LogAspect {
         long start = System.currentTimeMillis();
         Signature signature = joinPoint.getSignature();
         String param = JsonUtil.encode(joinPoint.getArgs());
-        try{
+        try {
             result = joinPoint.proceed();
             long end = System.currentTimeMillis();
-            logger.info("method={}||execute_time={}||param={}||result={}", signature.toString(), end-start, param, JsonUtil.encode(result));
-        }catch (Exception e) {
+            logger.info("method={}||execute_time={}||param={}||result={}", signature.toString(), end - start, param, JsonUtil.encode(result));
+        } catch (Exception e) {
             long end = System.currentTimeMillis();
-            logger.error("method={}||execute_time={}||param={}||error=", signature.toString(), end-start, param, e);
+            logger.error("method={}||execute_time={}||param={}||error=", signature.toString(), end - start, param, e);
             throw e;
         }
         return result;
