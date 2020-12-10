115ba3d23ee4a6d8961c8538ffc973a87acef199
luxiaoxun <luxiaoxun001@qq.com>
2020-12-10 14:50:29 +0800

    Update eagle-log

--- eagle-log/src/main/java/com/alarm/eagle/redis/LogStatWindowFunction.java
+++ eagle-log/src/main/java/com/alarm/eagle/redis/LogStatWindowFunction.java
@@ -3,6 +3,7 @@ package com.alarm.eagle.redis;
 import org.apache.flink.streaming.api.functions.windowing.ProcessWindowFunction;
 import org.apache.flink.streaming.api.windowing.windows.TimeWindow;
 import org.apache.flink.util.Collector;
+
 import java.util.Map;
 
 /**
@@ -11,12 +12,14 @@ import java.util.Map;
 public class LogStatWindowFunction extends ProcessWindowFunction<LogStatAccumulator, LogStatWindowResult, String, TimeWindow> {
     @Override
     public void process(String str, Context context, Iterable<LogStatAccumulator> iterable, Collector<LogStatWindowResult> collector) throws Exception {
-        LogStatAccumulator acc = iterable.iterator().next();
-        String key = acc.getKey();
-        long count = acc.getCount();
-        long startTime = context.window().getStart();
-        long endTime = context.window().getEnd();
-        Map<String, Long> ipMap = acc.getIpMap();
-        collector.collect(new LogStatWindowResult(key, count, startTime, endTime, ipMap));
+        if (iterable.iterator().hasNext()) {
+            LogStatAccumulator acc = iterable.iterator().next();
+            String key = acc.getKey();
+            long count = acc.getCount();
+            long startTime = context.window().getStart();
+            long endTime = context.window().getEnd();
+            Map<String, Long> ipMap = acc.getIpMap();
+            collector.collect(new LogStatWindowResult(key, count, startTime, endTime, ipMap));
+        }
     }
 }
